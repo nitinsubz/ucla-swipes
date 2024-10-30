2f0d9f9d61b63dbaac40dab8e804292bb8125202
Nitin Subramanian <nitins@ucla.edu>
2024-10-30 14:44:56 +0100

    Update get11PSwipes.js

--- app/endpoints/get11PSwipes.js
+++ app/endpoints/get11PSwipes.js
@@ -20,7 +20,7 @@ const get11PSwipes = (mockDate) => {
   let dateData = getStartDate();
   let startDate = dateData.startDate;
   let today = dateData.today;
-  const startingSwipes = 121;
+  const startingSwipes = 128;
 
   if (!mockDate && today.getTime() < startDate.getTime()) {
     return {
@@ -36,7 +36,7 @@ const get11PSwipes = (mockDate) => {
   }
 
 
-  var specialDates = {};
+  var specialDates = {'November 28, 2024' : 0, 'November 29, 2024': 0, 'November 30, 2024': 0, 'December 1, 2024': 1};
 
   let total = startingSwipes;
   while (startDate.toLocaleString('en-US', options) != todayDate) {
